31c542cee22d3c5b05cf1aceabe98b72b081e404
Alistair <alistairgarioch@gmail.com>
2021-11-23 17:42:01 +0000

    test(market): add tests for coin list and coin item + test setup changes

--- client/__test__/CoinItem.test.tsx
+++ client/__test__/CoinItem.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import CoinItem from '../components/MarketPage/CoinItem';
 
 const coinItemMock = {
@@ -11,11 +11,22 @@ const coinItemMock = {
   ath_date: 'Mon, 22 Nov 2021 03:04:00 -0500',
 };
 
+const openModalMock = jest.fn();
+
 test('news search renders correctly', () => {
   const { getByText } = render(
-    <CoinItem coinItem={coinItemMock} openModal={() => {}} />
+    <CoinItem coinItem={coinItemMock} openModal={openModalMock} />
   );
   expect(getByText(' Test ')).not.toBeNull();
   expect(getByText('$100')).not.toBeNull();
   expect(getByText('10.00%').props.style.color).toEqual('#34C759');
+});
+
+test('user can open the modal', () => {
+  const { getByText } = render(
+    <CoinItem coinItem={coinItemMock} openModal={openModalMock} />
+  );
+  const title = getByText(' Test ');
+  fireEvent.press(title);
+  expect(openModalMock).toHaveBeenCalledTimes(1);
 });
---
+++ client/__test__/CoinSearch.test.tsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { render, fireEvent } from '@testing-library/react-native';
+// import CoinSearch from '../components/MarketPage/CoinSearch';
+
+// jest.dontMock('react-native-gesture-handler');
+
+// test('search inputs sent to filter function', () => {
+//   const filterCoinsMock = jest.fn();
+//   const { getByPlaceholderText } = render(
+//     <CoinSearch input='' filterCoins={filterCoinsMock} />
+//   );
+//   fireEvent.changeText(getByPlaceholderText('Search...'), 'asdf');
+//   expect(filterCoinsMock).toHaveBeenCalled();
+// });
