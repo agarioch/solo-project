6db55194bc19d7a449b927e0aa45c85b51e8f39f
Alistair <alistairgarioch@gmail.com>
2021-11-23 09:50:53 +0000

    test(market): add CoinItem tests

--- client/__test__/CoinItem.test.tsx
+++ client/__test__/CoinItem.test.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import CoinItem from '../components/MarketPage/CoinItem';
 
+const coinItemMock = {
+  name: 'Test',
+  current_price: 100,
+  image: 'notavailable',
+  price_change_percentage_24h: 10,
+  sparkline_in_7d: { price: [70, 80, 90, 100] },
+  ath_date: 'Mon, 22 Nov 2021 03:04:00 -0500',
+};
+
 test('news search renders correctly', () => {
-  // const { getByText } = render(<CoinItem />);
-  // expect(getByText('Market Data Loading...')).not.toBeNull();
+  const { getByText } = render(
+    <CoinItem coinItem={coinItemMock} openModal={() => {}} />
+  );
+  expect(getByText(' Test ')).not.toBeNull();
+  expect(getByText('$100')).not.toBeNull();
+  expect(getByText('10.00%').props.style.color).toEqual('#34C759');
 });
