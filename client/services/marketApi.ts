b89aff8caf9494729ddbb808fd23d4652b014e0f
JIHYO-JEON <jihyo@gm.gist.ac.kr>
2021-11-24 13:10:30 +0000

    refactor(marketapi): delete some useless lines

--- client/services/marketApi.ts
+++ client/services/marketApi.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { coinData } from '../types/coinData';
 
 const API_URL =
   'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=7d';
