724d43b663d2b66b633aece01238f746c79db6e6
Alistair <alistairgarioch@gmail.com>
2021-11-24 08:43:37 +0000

    test(jest-setup): fix jest setup adding correct mocks

--- client/__test__/CoinSearch.test.tsx
+++ client/__test__/CoinSearch.test.tsx
@@ -1,14 +1,12 @@
-// import React from 'react';
-// import { render, fireEvent } from '@testing-library/react-native';
-// import CoinSearch from '../components/MarketPage/CoinSearch';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import CoinSearch from '../components/MarketPage/CoinSearch';
 
-// jest.dontMock('react-native-gesture-handler');
-
-// test('search inputs sent to filter function', () => {
-//   const filterCoinsMock = jest.fn();
-//   const { getByPlaceholderText } = render(
-//     <CoinSearch input='' filterCoins={filterCoinsMock} />
-//   );
-//   fireEvent.changeText(getByPlaceholderText('Search...'), 'asdf');
-//   expect(filterCoinsMock).toHaveBeenCalled();
-// });
+test('search inputs sent to filter function', () => {
+  const filterCoinsMock = jest.fn();
+  const { getByPlaceholderText } = render(
+    <CoinSearch input='' filterCoins={filterCoinsMock} />
+  );
+  fireEvent.changeText(getByPlaceholderText('Search...'), 'asdf');
+  expect(filterCoinsMock).toHaveBeenCalled();
+});
